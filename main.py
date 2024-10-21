0890936fdaf2f2dce499c17f68859b6609707322
Vova <VovaGu07@yandex.ru>
2024-10-22 00:35:58 +0400

    ``

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print(212)
+import json
+
+with open('in.json') as file:
+    data = json.load(file)
+
+for text in data:
+    if text['phoneNumber'][0]=="1" or text['phoneNumber'][0]=="+" and text['phoneNumber'][1]=="1":
+         if '4.0 Safari' in text['userAgent']:
+            print(text['name'] ,"  " ,text['phoneNumber'],"  ", text['userAgent'])
+
+# print(json.dumps(data, indent=4))
