915d21c49ef9c17e94a228425de84cc36c254d5a
Vova <VovaGu07@yandex.ru>
2024-10-22 22:55:40 +0400

    ``

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 import json
+import csv
+
 
 with open('in.json') as file:
     data = json.load(file)
 
-for text in data:
-    if text['phoneNumber'][0]=="1" or text['phoneNumber'][0]=="+" and text['phoneNumber'][1]=="1":
-         if '4.0 Safari' in text['userAgent']:
-            print(text['name'] ,"  " ,text['phoneNumber'],"  ", text['userAgent'])
+def gen_sqr(number):
+   for i in data:
+       if i['phoneNumber'][0] == "1" or i['phoneNumber'][0] == "+" and i['phoneNumber'][1] == "1":
+           if '4.0 Safari' in i['userAgent']:
+               yield i['name'], i['phoneNumber'], i['userAgent']
 
-# print(json.dumps(data, indent=4))
+a = gen_sqr
+
+with open("classmates.csv", mode="w", encoding='utf-8') as w_file:
+    file_writer = csv.writer(w_file, delimiter=",", lineterminator="\r")
+    for i in a(len(data)):
+        file_writer.writerow([i])
