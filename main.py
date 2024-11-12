3bc0cef494699db96dd9778995c58db875f4edb5
Vova <VovaGu07@yandex.ru>
2024-11-12 18:51:53 +0400

    1

--- main.py
+++ main.py
@@ -10,20 +10,21 @@ def read_json_file(file_path: str) -> list:
 
 def filter_users(users: list) -> Generator:
     for user in users:
-        if ('+1' in user.get('phoneNumber', '') or user.get('phoneNumber', '').startswith('1')) and '4.0 Safari' in user.get(
-                'userAgent', ''):# Проверка
+        if (('+1' in user.get('phoneNumber', '') or user.get('phoneNumber', '').startswith('1'))
+                and '4.0 Safari' in user.get('userAgent', '')):# Проверка
             yield user['name'], user['address'], user['email']
 
 
-def write_to_csv(file_path: str, rows: Generator[tuple[str, str, str], None, None]) -> None:
+def write_to_csv(file_path: str,
+                 rows: Generator[tuple[str, str, str], None, None]) -> None:
     with open(file_path, 'w', newline='', encoding='utf-8') as file:
         writer = csv.writer(file)
-        writer.writerow(['name', 'address', 'email'])  # заголовок
-        writer.writerows(rows)  # запись данных из генератора
+        writer.writerow(['name', 'address', 'email'])
+        writer.writerows(rows)
 
 
-log_file_path = "in.json"  # Путь к файлу с JSON-логами
-output_csv_path = "out.csv"  # Путь к выходному файлу CSV
+log_file_path = "in.json"
+output_csv_path = "out.csv"
 
 users_data = read_json_file(log_file_path)
 filtered_users = filter_users(users_data)
